266856c9b6ce0a37d59434156b9993e5c5e0628b
broc <bgarciacerv30@gmail.com>
2025-03-01 19:51:54 -0800

    start

--- my-react-app/src/components/Navbar.js
+++ my-react-app/src/components/Navbar.js
@@ -2,19 +2,23 @@ import React from "react";
 import { Link } from "react-router-dom";
 import '../styles/Navbar.css';
 
-function Navbar()
-{
-    return (
-        <div className={"navbar"}>
-            <div className={"leftSide"}>
-                <Link to="#home" className="nav-link">Home</Link>
-                <Link to="#about" className="nav-link">About</Link>
-                <Link to="#projects" className="nav-link">Projects</Link>
-                <Link to="#contact" className="nav-link">Contact</Link>
-            </div>
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="leftSide">
+        {/* Brand or logo name */}
+        <span className="brand">Broc Garcia-Cervantes</span>
+      </div>
 
-        </div>
-    );
+      <div className="rightSide">
+        {/* Links to scroll to specific sections */}
+        <Link to="#home" className="nav-link">Home</Link>
+        <Link to="#about" className="nav-link">About</Link>
+        <Link to="#projects" className="nav-link">Projects</Link>
+        <Link to="#contact" className="nav-link">Contact</Link>
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
--- my-react-app/src/pages/Home.js
+++ my-react-app/src/pages/Home.js
@@ -4,27 +4,11 @@ function Home()
 {
     return(
         <div>
-            <section id="home" className="section">
-                <h1>Welcome to My Portfolio</h1>
-                <p>Discover my work and learn more about me.</p>
+            <section id="home" className="home-section">
+                <h1 className="name-title">Welcome to my Portfolio</h1>
             </section>
 
-            <section id="about" className="section">
-                <h1>About Me</h1>
-                <p>I am a passionate web developer with experience in building dynamic websites.</p>
-            </section>
 
-            <section id="projects" className="section">
-                <h1>My Projects</h1>
-                <p>Here are some of the projects I've worked on:</p>
-                {/* Add project details */}
-            </section>
-
-            <section id="contact" className="section">
-                <h1>Contact Me</h1>
-                <p>If you want to get in touch, feel free to reach out!</p>
-                {/* Add contact form or details */}
-            </section>
         </div>
     )
 }
