df1c32f1b5eb2ce4014f4c306e4fe211d1bfa5cb
broc <bgarciacerv30@gmail.com>
2025-03-01 19:22:09 -0800

    start

--- my-react-app/src/components/Navbar.js
+++ my-react-app/src/components/Navbar.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import '../styles/Navbar.css';
 
 function Navbar()
 {
     return (
         <div className={"navbar"}>
-            <div className={"leftSide"}></div>
-            <div className={"rightSide"}></div>
+            <div className={"leftSide"}>
+                <Link to="#home" className="nav-link">Home</Link>
+                <Link to="#about" className="nav-link">About</Link>
+                <Link to="#projects" className="nav-link">Projects</Link>
+                <Link to="#contact" className="nav-link">Contact</Link>
+            </div>
+
         </div>
     );
 }
